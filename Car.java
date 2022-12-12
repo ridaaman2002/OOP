7057ac3b851c654ffc625f1f537bacf3d017ec41
ridaaman2002 <112433238+ridaaman2002@users.noreply.github.com>
2022-12-12 22:25:11 +0100

    create class car

---
+++ Car.java
@@ -0,0 +1,20 @@
+public class car {
+	//Atributs 
+	public String brand;
+	public String model;
+	public int amountOfFuel;
+	 
+	//constucteur 
+	public car() {
+		brand="";
+		model="";
+		amountOfFuel=0;
+		
+	}
+	public car(String brandX, String modelX, int amountX) {
+		brand=brandX;
+		model=modelX;
+		amountOfFuel=amountX;
+	}
+	
+}
